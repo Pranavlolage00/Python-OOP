6ab0906f63c63d9e5c71a40c4881d1c34e18274d
Pranav G. Lolage <129136237+Pranavlolage00@users.noreply.github.com>
2024-07-05 21:16:27 +0530

    Add files via upload

---
+++ Function_add.py
@@ -0,0 +1,7 @@
+def add(a,b):
+    c=a+b
+    print("addition=",c)
+    return
+
+add(20,20)
+add(b=78,a=89)
---
+++ default_Arg.py
@@ -0,0 +1,6 @@
+def add(a=10,b=20,c=50):
+    ans=a+b+c
+    print("Addition=",ans-+)
+    return
+
+add(45,60)
